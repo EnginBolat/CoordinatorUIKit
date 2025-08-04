6a00611ce93f69bc70b4f7fed6ae913aa73e1818
Engin Bolat <engi.bolat@gmail.com>
2025-08-04 22:28:06 +0300

    Initial Commit

---
+++ CoordinatorUIKitTests/CoordinatorUIKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorUIKitTests.swift
+//  CoordinatorUIKitTests
+//
+//  Created by Engin on 4.08.2025.
+//
+
+import Testing
+@testable import CoordinatorUIKit
+
+struct CoordinatorUIKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
